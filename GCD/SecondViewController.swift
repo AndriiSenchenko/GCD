420f2d77376a9828ec4a0387be64024669c22326
Andrii <andre.senchenko@gmail.com>
2023-03-02 10:31:22 +0200

    Apply GCD

--- GCD/SecondViewController.swift
+++ GCD/SecondViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     @IBOutlet weak var imageView: UIImageView!
     fileprivate var imageURL: URL?
     fileprivate var image: UIImage? {
@@ -17,6 +18,8 @@ class SecondViewController: UIViewController {
         } set {
             imageView.image = newValue
             imageView.sizeToFit()
+            activityIndicator.startAnimating()
+            activityIndicator.isHidden = true
         }
     }
     
@@ -24,9 +27,17 @@ class SecondViewController: UIViewController {
         super.viewDidLoad()
         fetchImage()
     }
+    
     fileprivate func fetchImage() {
         imageURL = URL(string: "https://upload.wikimedia.org/wikipedia/commons/0/07/Huge_ball_at_Vilnius_center.jpg")
-        guard let url = imageURL, let imageData = try? Data(contentsOf: url) else { return }
+        activityIndicator.isHidden = false
+        activityIndicator.startAnimating()
+        let queue = DispatchQueue.global(qos: .utility)
+        queue.async {
+            guard let url = self.imageURL, let imageData = try? Data(contentsOf: url) else { return }
+            DispatchQueue.main.async {
                 self.image = UIImage(data: imageData)
+                }
             }
         }
+    }
